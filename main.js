abb0f295653358fd3c51a434d79006567ea3c526
mynorzs <72713900+mynorzs@users.noreply.github.com>
2023-11-12 20:13:26 -0600

    Add operation functionality
    Added the functionality of each operator, as well as the logic
    to display the numbers correctly when more than 2 operands are
    considered. No floating numbers are fully suported yet.

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ const equalButton = document.querySelector("#equal-button");
 const clearButton = document.querySelector("#clear-button");
 
 let first;
-let second;
+let equal = false;
+let operator;
 
 // Here I clear the display and return the first and second valures to undefined.
 function clearDisplay() {
   display.textContent = "0";
   first = undefined;
-  second = undefined;
+  second = false;
+  operator = undefined;
 }
 
 function operate(operator, firstOperand, secondOperand) {
@@ -29,26 +31,47 @@ function operate(operator, firstOperand, secondOperand) {
   }
 }
 
-function printNumber(button) {
-  if (display.textContent === "0") {
-    if (button.textContent !== "0") {
-      display.textContent = button.textContent;
-    }
+function printNumber(content) {
+  if (display.textContent === "0" || operator || equal) {
+    display.textContent = content;
   } else {
-    display.textContent += button.textContent;
+    display.textContent += content;
   }
 }
 
 numberButton.forEach(function (button) {
   button.addEventListener("click", (e) => {
-    printNumber(e.target);
+    printNumber(e.target.textContent);
   });
 });
 
 clearButton.addEventListener("click", () => clearDisplay());
 
-if (first) {
-  console.log(first);
-} else {
-  console.log("no hay valor");
-}
+// After the operator button is called, the current displayed number has to be stored in first.
+operatorButton.forEach(function (button) {
+  button.addEventListener("click", (e) => {
+    if (operator) {
+      display.textContent = operate(
+        operator,
+        first,
+        Number(display.textContent)
+      );
+      first = Number(display.textContent);
+    } else {
+      first = Number(display.textContent);
+    }
+    operator = e.target.textContent;
+  });
+});
+
+equalButton.addEventListener("click", () => {
+  if (first && operator) {
+    display.textContent = operate(operator, first, Number(display.textContent));
+    first = undefined;
+    operator = undefined;
+    equal = true;
+  }
+});
+// The operator has to be displayed and stored to operator.
+// After the second number, a new operator or the equal operator should display the result of the previous operation.
+// The result has to be first now and second has to be undefined.
